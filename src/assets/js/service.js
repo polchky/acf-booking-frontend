10fbce81b95634dfe921b3161b88832edfae5c66
Pascal Gremaud <pascal.gremaud@unifr.ch>
2020-10-06 13:30:29 +0200

    add config.json

--- src/assets/js/service.js
+++ src/assets/js/service.js
@@ -1,6 +1,7 @@
+const config = require('../../../config.json');
 import axios from 'axios';
 
-axios.defaults.baseURL = process.env.VUE_APP_BACKEND_URL;
+axios.defaults.baseURL = config.serverURL;
 
 export default {
 
